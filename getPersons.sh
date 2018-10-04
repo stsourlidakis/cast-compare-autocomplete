c505ac3f75ff7221a160eecdb8f1377092f956f3
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2018-10-04 15:12:24 +0300

    create getPersons bash script

---
+++ getPersons.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# https://developers.themoviedb.org/3/getting-started/daily-file-exports
+filename="person_ids_$(date +%m_%d_%Y).json"
+fileUrl="http://files.tmdb.org/p/exports/$filename.gz"
+wget $fileUrl
+gunzip -f "$filename.gz"
+
+echo "Starting data processing"
+
+sed -r 's/^\{"adult":|\}$//g; s/,"id":|,"name":"|","popularity":/\t/g' $filename |
+sort -t$'\t' -rnk4 |
+awk -F"\t" '$1 == "false" { print $3"\t"$2 }' > persons.tsv
+
+echo "Persons file created"
