7c73b9131cbf025cb44ef4294ec95f7f4c420661
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2018-10-15 17:36:46 +0300

    add autocomplete endpoint for movies

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const Autocomplete = require('./Autocomplete');
 const port = 3001;
 
 const person = new Autocomplete('person', 'person.tsv');
+const movie = new Autocomplete('movie', 'movie.tsv');
 
 app.use(cors());
 app.disable('x-powered-by');
-app.use('/', person.router);
+app.use('/person', person.router);
+app.use('/movie', movie.router);
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`)
