0273abae63e253411e92bfed822f77b6b3a758bf
stsourlidakis <s.tsourlidakis@hotmail.com>
2025-08-09 15:23:04 +0100

    feat: add min popularity filter to dataset creation

--- getMovieData.sh
+++ getMovieData.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # https://developers.themoviedb.org/3/getting-started/daily-file-exports
+MIN_POPULARITY=0.1
 filename="movie_ids_$(date +%m_%d_%Y).json"
 fileUrl="http://files.tmdb.org/p/exports/$filename.gz"
 wget $fileUrl
@@ -8,6 +9,7 @@ gunzip -f "$filename.gz"
 echo "Starting data processing"
 
 sed -r 's/^\{"adult":|\}$//g; s/,"id":|,"original_title":"|","popularity":|,"video":/\t/g' $filename |
+awk -F"\t" -v min_pop="$MIN_POPULARITY" '$4 >= min_pop' |
 sort -t$'\t' -rnk4 |
 awk -F"\t" '$1 == "false" { print $3"\t"$2 }' > movie.tsv
 
--- getPersonData.sh
+++ getPersonData.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # https://developers.themoviedb.org/3/getting-started/daily-file-exports
+MIN_POPULARITY=0.8
 filename="person_ids_$(date +%m_%d_%Y).json"
 fileUrl="http://files.tmdb.org/p/exports/$filename.gz"
 wget $fileUrl
@@ -8,6 +9,7 @@ gunzip -f "$filename.gz"
 echo "Starting data processing"
 
 sed -r 's/^\{"adult":|\}$//g; s/,"id":|,"name":"|","popularity":/\t/g' $filename |
+awk -F"\t" -v min_pop="$MIN_POPULARITY" '$4 >= min_pop' |
 sort -t$'\t' -rnk4 |
 awk -F"\t" '$1 == "false" { print $3"\t"$2 }' > person.tsv
 
